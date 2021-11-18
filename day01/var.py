571c0d0396d053f6561f7665475d4b66a5081017
sugu2100 <sugu2100@daum.net>
2021-11-19 08:56:32 +0900

    git test

--- day01/var.py
+++ day01/var.py
@@ -8,14 +8,13 @@ name = '한글'
 phone = '010-1234-5678'
 age = 35
 
-"""
 print("아이디 :", userid)
 print("비밀번호 :", userpw)
 print("이름 :", name)
 print("전화번호 :", phone)
 print("나이 :", age)
-"""
 
+'''
 print("="*30)
 print("아이디 : " + userid)
 print("비밀번호 : " + userpw)
@@ -23,5 +22,6 @@ print("이름 : " + name)
 print("전화번호 : " + phone)
 print("나이 :" + str(age))    
 print("="*30)
+'''
 #숫자는 문자 자료형으로 형변환 - str() 함수 사용
 
